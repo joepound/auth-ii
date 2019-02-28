62b8eb8ace7c43fe6075014aa556ed3f56d303c8
joepound <14357797+joepound@users.noreply.github.com>
2019-02-28 11:36:04 -0700

    Initial setup for status messages for users endpoints

---
+++ middleware/routes/users/statusMsgs.js
@@ -0,0 +1,12 @@
+module.exports = {
+  registerUser: [
+    "\nAttempting to register new user...",
+    "Checking if all required fields for registration were supplied...",
+    "User registration attempt finished."
+  ],
+  login: [
+    "\nAttempting login...",
+    "Checking if all required fields for login were supplied...",
+    "User login attempt finished."
+  ]
+};
