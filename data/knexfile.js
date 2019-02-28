fa1694f5f9714df541032950a7770ff8c9057f3c
joepound <14357797+joepound@users.noreply.github.com>
2019-02-28 15:16:40 -0700

    Fixed incorrect names in knexfile.js comments

--- data/knexfile.js
+++ data/knexfile.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 pg.defaults.ssl = true;
 
 module.exports = {
-  // For Knex CLI only (use config named "server" for actual dev use) - need to do this for file pathing
+  // For Knex CLI only (use config named "devServer" for actual dev use) - need to do this for file pathing
   development: {
     client: "pg",
     connection: process.env.DATABASE_URL,
@@ -19,7 +19,6 @@ module.exports = {
     seeds: {
       directory: "./seeds"
     }
-    // Will not turn on foreign key constraints to enable deletion (this is needed to allow seeds to run)
   },
 
   // For actual dev use
